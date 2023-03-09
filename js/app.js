45fef4778b2daae41e4f23c585e930a6c33fcca8
Alejandro Medici <alejandro.medici@nextiteration.com.ar>
2023-03-09 20:29:38 -0300

    ejericios de clase 04

--- js/app.js
+++ js/app.js
@@ -13,12 +13,37 @@ const myCat = {
 console.log(myDog.name);
 console.log(myCat.name);
 
-
+/*
 class Mouse {
     constructor(name, age) {
         this.name = name;
         this.age = age;
     }
+}*/
+
+
+function Mouse(name, age) {
+    this.name = name;
+    this.age = age;
 }
 
 const myMouse = new Mouse("Perez", 4);
+
+class Button {
+    constructor(parentID, text) {
+        this.parentID = parentID;
+        // completa this.text
+    }
+
+    render(){
+        let myApp = document.getElementById(this.parentID);
+
+        myApp.innerHTML = ""; // VAmos a escribir el html para que se 
+        // dibuje un boton con el texto de this.text
+    }
+}
+
+// Construir el objeto boton
+// y llamar al metodo render de ese objeto.
+// let myButon = new ....
+// myButon.metodoQueSea()
