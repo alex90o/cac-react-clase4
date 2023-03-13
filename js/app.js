00fce5673a23bf763aed4eb61780f553d606d8c9
Alexis <alex10o@hotmail.com>
2023-03-13 09:12:36 -0300

    se cargaron cosa de js anteriores

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
+var a = 120;
+var b =550;
+
+console.log(a+b);
+
+alert(" ♥♥♥ Saludar ♦♦♦")
+ 
+
+ var nombre = prompt("Ingrese su nombre")
+
+ var apellido = prompt("Ingrese su apellido")
+
+ //alert("Hola " + nombre +" "+ apellido + " bienvenido que tengas un buen día"); 
+ let myDOM = document; 
+
+console.log(myDOM);
+
+let myApp = document.getElementById("myApp");
+
+myApp.innerHTML = "Hola " + nombre +" "+ apellido + " bienvenido que tengas un buen día"
+
+/* clase 4*/
 let myDom = document; //document es un objeto global
 
 let name = "Firlais";
